9fbe24b800a043606ff1298e50957ad8f4964f20
dmsumon2020 <dmsumon2020@gmail.com>
2025-05-30 01:22:18 +0600

    added copyright

--- src/components/Footer/Copyright.jsx
+++ src/components/Footer/Copyright.jsx
@@ -1,8 +1,8 @@
 export default function Copyright() {
   return (
-    <div className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
+    <div className="bg-[#f8f8f8] px-32 footer sm:footer-horizontal text-base-content p-4 border-t border-[#D9D9D9] text-left">
       <aside>
-        <p>
+        <p className="rubik-font-class font-medium text-[15px]">
           Copyright © {new Date().getFullYear()} - All right reserved by ACME
           Industries Ltd
         </p>
