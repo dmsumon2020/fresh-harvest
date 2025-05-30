7dbda86273b97a6563cd8ac4620f1cf4fb9811fd
dmsumon2020 <dmsumon2020@gmail.com>
2025-05-30 09:52:20 +0600

    final3

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -7,13 +7,13 @@ const blogs = [
       "10 Essential Tips for Building Fast and Responsive Web Applications",
   },
   {
-    image: "/assets/images/blog1.jpg",
+    image: "/assets/images/blog2.jpg",
     date: "April 10, 2025",
     title:
       "Understanding JavaScript Closures and Their Use Cases in Real Projects",
   },
   {
-    image: "/assets/images/blog1.jpg",
+    image: "/assets/images/blog3.jpg",
     date: "May 5, 2025",
     title:
       "How to Optimize Your React Applications for Speed, Scalability, and UX",
